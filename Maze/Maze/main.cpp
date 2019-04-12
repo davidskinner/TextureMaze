6edd8b6509fb228ea30a3ffa2cff6ba2d14370ea
Unknown <dskinner@uark.edu>
2019-04-12 11:27:00 -0500

    yay starting over

---
+++ Maze/Maze/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Maze
+//
+//  Created by David Skinner on 4/12/19.
+//  Copyright © 2019 David Skinner. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
